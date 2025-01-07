347dee2a52880187df96a0e96986f36ac9938fc0
Chirana <engsw.thiago@gmail.com>
2025-01-06 23:00:50 -0300

    feat: add field to user to represent his Shopowner, if self is Salesperson

--- db/migrate/20250106073845_create_users.rb
+++ db/migrate/20250106073845_create_users.rb
@@ -6,6 +6,8 @@ class CreateUsers < ActiveRecord::Migration[8.0]
       t.string :email, null: false
       t.string :password_digest, null: false
 
+      t.references :shopowner, null: true, foreign_key: { to_table: :users }
+
       t.timestamps
     end
     add_index :users, :email, unique: true
