0a3db6189aae09b066a3e434c080b5d65136ef99
Chirana <engsw.thiago@gmail.com>
2025-01-08 00:04:16 -0300

    feat: creating salesperson controller and his access

--- app/controllers/salesperson_controller.rb
+++ app/controllers/salesperson_controller.rb
@@ -1,3 +1,12 @@
 class SalespersonController < ApplicationController
-  allow_to_salesperson
+  allow_to_salespersons
+
+  def dashboard
+    render plain: "This is a dash to only salesperson"
+  end
+
+  def payments
+    pays = current_user.payments
+    render json: { payments: pays }
+  end
 end
