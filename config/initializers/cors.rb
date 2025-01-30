0a1d884f2cbeb3920aa2de990bad5a293ee99d5d
Chirana <engsw.thiago@gmail.com>
2025-01-29 23:28:34 -0300

    feat: adjust cors

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,11 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins "localhost:3005",
-    "localhost:3000",
-    "0.0.0.0:3005",
-    "0.0.0.0:3000",
-    "the-store.devcurumin.com.br"
+    origins "*"
 
     resource "*",
       headers: :any,
